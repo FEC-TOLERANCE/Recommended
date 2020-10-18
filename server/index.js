96856a80b885686d913db0817f96e97ddea276f9
Teddy Kim <teddykim09@gmail.com>
2020-10-18 13:41:33 -0700

    Added further dependencies and adjust client component as well as webpack entry to match

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use('/:projectId', express.static(__dirname + '/../client/dist'));
+
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
-})
+});
