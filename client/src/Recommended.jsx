80571224738c9e27803b84afde971dd7a1892eb3
Teddy Kim <teddykim09@gmail.com>
2020-10-18 13:46:12 -0700

    Set up API request for data to display for component

--- client/src/Recommended.jsx
+++ client/src/Recommended.jsx
@@ -4,6 +4,24 @@ import axios from 'axios';
 
 function Recommended() {
 
+  useEffect(() => {
+    let currentUrl = window.location.href.split('/');
+    let projectId = currentUrl[3];
+
+    axios.get(`http://localhost:3004/funding/${projectId}`)
+      .then(projectData => {
+        console.log(projectData.data);
+      })
+      .catch(err => {
+        throw new Error(err);
+      })
+  }, []);
+
+  return (
+    <div>
+      Recommended
+    </div>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Recommended />, document.getElementById('recommended'));
